bdb368d9a33c25db690eae2877a8e765dfd8e925
Xander Cole <65436489+Clackroe@users.noreply.github.com>
2023-01-24 14:46:24 -0500

    Finished Lab3?

--- Software Engineering/w2/lab3/pygameGUI.py
+++ Software Engineering/w2/lab3/pygameGUI.py
@@ -1,10 +1,10 @@
 # from polygons import Polygons
 import math
 import pygame
-import polygons
+from polygons import Polygons
 
 
-class PygameGUI(polygons):  # Something is missing here
+class PygameGUI(Polygons):  # Something is missing here
     # TODO 7: Fix this class so that it inherits from the Polygons class
 
     def __init__(self, num_sides, side_length):
