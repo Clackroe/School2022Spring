9fb748fe9cbc2fffa97996388f9e91db3ce22b0e
Clackroe <clackroe@icloud.com>
2023-01-31 21:56:32 -0500

    Edited itemTests

--- Software Engineering/w4/lab5/itemTests.py
+++ Software Engineering/w4/lab5/itemTests.py
@@ -110,9 +110,9 @@ def test_calculate_item_total_price_converts_discount(expected_discount: float =
     # - Compare the result to the expected_discount value
     # - Run the test before implementing the calculate_item_total_price method and check that it fails
     # - TIP: Use the round function to round the result to 2 decimal places
-    item = None  # Replace None with the correct code
-    fail_case = None  # Replace None with the correct code
-    actual_discount = None  # Replace None with the correct code
+    item = Item("Item", 1, 2, expected_discount)  # Replace None with the correct code
+    fail_case =  # Replace None with the correct code
+    actual_discount =  # Replace None with the correct code
 
     if fail_case:
         return (False, f"Error in test_calculate_item_price_converts_discount: Incorrect discount\n  - Expected: {expected_discount}\n  - Actual: {actual_discount}")
