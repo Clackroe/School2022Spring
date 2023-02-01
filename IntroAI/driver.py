015ea35eeb648df0e56c131a7b88ce968eb72419
Xander Cole <clackroe@icloud.com>
2023-02-01 02:28:26 -0500

    Finished Queen Display and Generation

--- IntroAI/driver.py
+++ IntroAI/driver.py
@@ -1,5 +1,54 @@
 
-from queens import Queens as q
+from queens import Queen as q
 
-h_value = 0
+import numpy as np
+
+h_value = 0 #heuristic value
+
+#board setup
+board_size = 8;
+board = np.zeros((board_size, board_size), dtype=int) # initial board set to zeros
+
+#queen setup
+qlist:q = []
+
+
+def main():
+    initialize_queens()
+    
+    generate_queens()
+    
+    
+    print_board()
+
+
+
+
+#print the board in a correct format
+def print_board():
+    for i in reversed(range(board_size)):
+        for j in range(board_size):
+            if (j==board_size-1):
+                print(board[j][i], end="")
+            else:
+                print(f"{board[j][i]},", end=" ")
+        print()
+   
+def generate_queens():
+    for qt in qlist:
+        qt.generate_pos()
+        board[qt.position[0], qt.position[1]] = 1
+        
+
+
+def initialize_queens():
+    for i in range(board_size):
+        qlist.append(q(i, 0))
+
+
+if __name__ == '__main__':
+    main()
+    
+
+
 
--- IntroAI/queens.py
+++ IntroAI/queens.py
@@ -1,20 +1,28 @@
+import random as rand
+
 class Queen:
     
-    position = [int]
+    position = [0, 0]
     
-def compareQueens(self, other : Queen) -> bool:
-    x = other.position[0]
-    y= other.position[1]
+    def __init__(self, x:int, y:int):
+       self.position = [x, y]
     
-    
-    if (y == self.position[1] or x == self.position[0]):
-        return True
-    
-    elif (y-self.position[1] == x-self.position[0] or self.position[1]-y == self.position[0]-x):
-        return True
-    else:
-        return False
+    def compare_queens(self, other) -> bool:
+        x = other.position[0]
+        y= other.position[1]
         
+        
+        if (y == self.position[1] or x == self.position[0]):
+            return True
+        
+        elif (y-self.position[1] == x-self.position[0] or self.position[1]-y == self.position[0]-x):
+            return True
+        else:
+            return False
+
+
+    def generate_pos(self):
+        self.position[1] = rand.randint(0, 7)
                     
 
     
