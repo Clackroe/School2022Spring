4dc4cd4994182763c323cf7dad96424ce7ec3cdd
Xander Cole <clackroe@icloud.com>
2023-02-14 14:04:27 -0500

    Stuff

--- IntroAI/NewQueen/driver.py
+++ IntroAI/NewQueen/driver.py
@@ -1,9 +1,73 @@
 
 
+# import time
+# from queens import Queen
+# from board import Board
+
+
+# curr_state = Board([])
+
+# goal_score = 0
+
+# num_states = 0
+# num_neighbors = 0
+
+
+
+# curr_state.start_random()
+
+
+# curr_state.produce_neighbor
+            
+        
+    
+        
+                
+
+
+# curr_state.print_board()
+# print(f"Solution Found!")
+# print(f"States Changed: {num_neighbors}")
+# print(f"Num Restarts: {num_states}")
 import time
 from queens import Queen
 from board import Board
 
+
+def hill_climbing(state):
+    global num_states, num_neighbors
+    
+    num_states = 0
+    num_neighbors = 0
+    
+    # Initialize the current state
+    current = state
+    current.calculate_score()
+    num_states += 1
+    
+    while True:
+        # Generate all neighbors of the current state
+        neighbors = []
+        for i in range(Board.length):
+            for j in range(Board.length):
+                if current.qlist[i].posy != j:
+                    neighbors.append(current.produce_neighbor(i, j))
+        
+        # Calculate the scores of all the neighbors
+        for neighbor in neighbors:
+            neighbor.calculate_score()
+            num_neighbors += 1
+        
+        # Choose the best neighbor as the new current state
+        best = min(neighbors, key=lambda x: x.score)
+        
+        if best.score >= current.score:
+            # No better neighbor, return the current state as the solution
+            return current
+        else:
+            # Update the current state with the best neighbor
+            current = best
+            num_states += 1
 
 curr_state = Board([])
 
@@ -12,31 +76,15 @@ goal_score = 0
 num_states = 0
 num_neighbors = 0
 
-
-
 curr_state.start_random()
-
-
-while (True):
-    for q in range(curr_state.length):
-        
-        for r in range(curr_state.length):
-            temp = curr_state.produce_neighbor(q, r)
-            temp.calculate_score()
-            temp.print_board()
-    break
-            
-        
-    
-        
-                
-
-
 curr_state.print_board()
-print(f"Solution Found!")
-print(f"States Changed: {num_neighbors}")
-print(f"Num Restarts: {num_states}")
 
+hill_climbing(curr_state)
+    
+
+print(f"Solution Found!")
+print(f"States Changed: {num_states}")
+print(f"Num Neighbors Checked: {num_neighbors}")
 
 
 
