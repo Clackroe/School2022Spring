04cfb652beeedb6ce9fa8b3ecbf157565460d543
Xander Cole <clackroe@icloud.com>
2023-02-16 15:21:40 -0500

    YES

--- WebAppDesign/Activity6/Cole-Activity6.js
+++ WebAppDesign/Activity6/Cole-Activity6.js
@@ -8,5 +8,10 @@ function pressed(){
     
     let gradeNum = prompt("What is your grade? (Enter a number 0-120 | or enter -1 to end)");
 
+    switch(gradeNum){
 
+        case -1:
+            
+
+    }
 }
