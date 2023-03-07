183c7cb285e300dc8d77a0656ded3e2f5c5d23a7
Clackroe <clackroe@icloud.com>
2023-03-07 12:50:41 -0500

    Finished first draft | LewisThrifts

---
+++ WebAppDesign/Lewis Thrifts/thrifts.js
@@ -0,0 +1,18 @@
+const $ = selector => document.querySelector(selector);
+
+document.addEventListener("DOMContentLoaded", () => {
+
+
+    document.querySelectorAll('.btn_buy').forEach(item =>{
+        item.addEventListener('click', event => {
+            alert("WIP | Functionality Coming Soon");
+        })
+    })
+
+    document.querySelectorAll('.social-icon').forEach(item =>{
+        item.addEventListener('click', event => {
+            alert("WIP | Functionality Coming Soon");
+        })
+    })
+
+});
