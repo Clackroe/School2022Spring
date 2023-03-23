eabc04a8aefcdebc19d60a27337257b627aef9aa
Xander Cole <clackroe@icloud.com>
2023-03-23 14:21:09 -0400

    js

---
+++ WebAppDesign/swap/swap.js
@@ -0,0 +1,28 @@
+"use strict";
+$(document).ready( () => {
+
+    // preload images
+    $("#image_list a").each( (index, link) => {
+        const image = new Image();
+        image.src = link.href;
+    });
+
+    // set up event handlers for links    
+    $("#image_list a").click( evt => {
+        // get clicked <a> tag
+        const link = evt.currentTarget;
+        
+        // swap image
+        $("#main_image").attr("src", link.href);
+        
+        //swap caption
+        $("#caption").text(link.title);
+                
+        // cancel the default action of the link
+        evt.preventDefault();
+    });
+
+    // move focus to first thumbnail
+    $("li:eq(2) a").focus();
+    $("li:eq(2) a").trigger("click");
+});
